9ccbbce8e9200f68930892104411d23dca05b3f3
AryamanSingh <aryamansingh2803@gmail.com>
2022-03-20 01:40:16 +0530

    ts particle bg

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components/macro'
 import { useNav } from '../hooks/useNav'
 import { Fade } from 'react-awesome-reveal'
-
+import ParticleConfig from './particle-config'
+import Particles from 'react-tsparticles'
 const StyledAboutMeWrapper = styled.section`
   /* border: 1px solid red; */
   width: 80%;
@@ -229,227 +230,321 @@ const StyledLogos = styled.div`
 
 const AboutMe = () => {
   const aboutRef = useNav('About')
+
+  const particlesInit = (main) => {
+    console.log(main)
+  }
+  const particlesLoaded = (container) => {
+    console.log(container)
+  }
   return (
     <StyledAboutMeWrapper>
       <StyledAboutMe id='aboutContainer' ref={aboutRef}>
-        <H1>About Me</H1>
+        <div style={{ position: 'relative' }}>
+          <Particles
+            id='tsparticles'
+            init={particlesInit}
+            loaded={particlesLoaded}
+            width='95vw'
+            style={{
+              position: 'absolute',
+              marginLeft: '-7rem',
+            }}
+            options={{
+              fpsLimit: 60,
+              fullScreen: { enable: false, zIndex: 330 },
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: 'push',
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: 'repulse',
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 20,
+                  },
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: '#7a7a7a',
+                },
+                links: {
+                  color: '#ababab',
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.3,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: 'none',
+                  enable: true,
+                  outMode: 'bounce',
+                  random: false,
+                  speed: 2,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    area: 1080,
+                  },
+                  value: 50,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: 'circle',
+                },
+                size: {
+                  random: true,
+                  value: 3,
+                },
+              },
+              detectRetina: true,
+            }}
+          />
 
-        <StyledContainer>
-          <StyledLeft>
-            <Fade cascade>
-              <Article>
-                I&apos;m a front-end developer from the city of Nawabs Lucknow,
-                India.
-                <br />I enjoy turning complex problems into simple, beautiful
-                and intuitive designs. When I'm not pushing pixels, you'll find
-                me scrolling twitter checking out <em>#ReactJS posts</em>,
-                playing with my dogs Snowy &nbsp;
-                <img
-                  src='images/snowy.PNG'
-                  height='20px'
-                  width='auto'
-                  alt='snowy - Dog'
-                  aria-label='snowy - Dog'
-                />
-                &nbsp; &amp; Simba&nbsp;
-                <img
-                  src='images/simba.PNG'
-                  height='20px'
-                  width='auto'
-                  alt='Simba - Dog'
-                  aria-label='simba - Dog'
-                />
-                &nbsp; or eating probably.
-              </Article>
-              <Fade delay={300}>
-                <H3Left>Experience &amp; Education</H3Left>
+          <H1>About Me</H1>
+
+          <StyledContainer>
+            <StyledLeft>
+              <Fade cascade>
+                <Article>
+                  I&apos;m a front-end developer from the city of Nawabs
+                  Lucknow, India.
+                  <br />I enjoy turning complex problems into simple, beautiful
+                  and intuitive designs. When I'm not pushing pixels, you'll
+                  find me scrolling twitter checking out <em>#ReactJS posts</em>
+                  , playing with my dogs Snowy &nbsp;
+                  <img
+                    src='images/snowy.PNG'
+                    height='20px'
+                    width='auto'
+                    alt='snowy - Dog'
+                    aria-label='snowy - Dog'
+                  />
+                  &nbsp; &amp; Simba&nbsp;
+                  <img
+                    src='images/simba.PNG'
+                    height='20px'
+                    width='auto'
+                    alt='Simba - Dog'
+                    aria-label='simba - Dog'
+                  />
+                  &nbsp; or eating probably.
+                </Article>
+                <Fade delay={300}>
+                  <H3Left>Experience &amp; Education</H3Left>
+                </Fade>
+                <StyledExp>
+                  <StyledUl>
+                    <StyledLi>
+                      <Fade>
+                        <StyledLiContent>
+                          <div>Full Stack Developer Intern</div>
+                          <p>Cognizant Technology Solutions</p>
+                          <span>Jan 2022- </span>
+                        </StyledLiContent>
+                      </Fade>
+                    </StyledLi>
+                    <StyledLi>
+                      <Fade delay={1150}>
+                        <StyledLiContent>
+                          <div>Project Intern</div>
+                          <p>Phemsoft Software Pvt Ltd</p>
+                          <span>Jun 2021- Aug 2021</span>
+                        </StyledLiContent>
+                      </Fade>
+                    </StyledLi>
+                    <StyledLi>
+                      <Fade delay={1250}>
+                        <StyledLiContent>
+                          <div>Social Intern</div>
+                          <p>Brij Rani Charitable Trust</p>
+                          <span>Jun 2020- Jul 2020</span>
+                        </StyledLiContent>
+                      </Fade>
+                    </StyledLi>
+                    <StyledLi>
+                      <Fade delay={1350}>
+                        <StyledLiContent>
+                          <div>
+                            University Of Petroleum &amp; Energy Studies
+                          </div>
+                          <p>B.Tech in Computer Science Engineering</p>
+                          <span>Aug 2018- </span>
+                        </StyledLiContent>
+                      </Fade>
+                    </StyledLi>
+                  </StyledUl>
+                </StyledExp>
               </Fade>
-              <StyledExp>
-                <StyledUl>
-                  <StyledLi>
-                    <Fade>
-                      <StyledLiContent>
-                        <div>Full Stack Developer Intern</div>
-                        <p>Cognizant Technology Solutions</p>
-                        <span>Jan 2022- </span>
-                      </StyledLiContent>
-                    </Fade>
-                  </StyledLi>
-                  <StyledLi>
-                    <Fade delay={1150}>
-                      <StyledLiContent>
-                        <div>Project Intern</div>
-                        <p>Phemsoft Software Pvt Ltd</p>
-                        <span>Jun 2021- Aug 2021</span>
-                      </StyledLiContent>
-                    </Fade>
-                  </StyledLi>
-                  <StyledLi>
-                    <Fade delay={1250}>
-                      <StyledLiContent>
-                        <div>Social Intern</div>
-                        <p>Brij Rani Charitable Trust</p>
-                        <span>Jun 2020- Jul 2020</span>
-                      </StyledLiContent>
-                    </Fade>
-                  </StyledLi>
-                  <StyledLi>
-                    <Fade delay={1350}>
-                      <StyledLiContent>
-                        <div>University Of Petroleum &amp; Energy Studies</div>
-                        <p>B.Tech in Computer Science Engineering</p>
-                        <span>Aug 2018- </span>
-                      </StyledLiContent>
-                    </Fade>
-                  </StyledLi>
-                </StyledUl>
-              </StyledExp>
-            </Fade>
-          </StyledLeft>
-          <StyledRight>
-            <H3>Skills</H3>
-            <StyledLogosWrapper>
-              <Fade cascade damping='0.25'>
-                <a
-                  href='https://developer.mozilla.org/en-US/docs/Glossary/HTML5'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/html5.svg'
-                      alt='html'
-                      aria-label='html icon'
-                      tabIndex='0'
-                    />
-                    <p>Html5</p>
-                  </StyledLogos>
-                </a>
-                <a
-                  href='https://developer.mozilla.org/en-US/docs/Web/CSS'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/css3.svg'
-                      alt='css3'
-                      aria-label='css3 icon'
-                      tabIndex='0'
-                    />
-                    <p>CSS3</p>
-                  </StyledLogos>
-                </a>
-                <a
-                  href='https://www.javascript.com/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/javascript.svg'
-                      alt='javascript'
-                      aria-label='javascript icon'
-                      tabIndex='0'
-                    />
-                    <p>JavaScript</p>
-                  </StyledLogos>
-                </a>
-                <a href='https://reactjs.org/' target='_blank' rel='noreferrer'>
-                  <StyledLogos>
-                    <img
-                      src='images/react.svg'
-                      alt='react'
-                      aria-label='react icon'
-                      tabIndex='0'
-                    />
-                    <p>React</p>
-                  </StyledLogos>
-                </a>
-                <a
-                  href='https://www.mongodb.com/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/mongodb.svg'
-                      alt='mongodb'
-                      aria-label='mongodb icon'
-                      tabIndex='0'
-                    />
-                    <p>MongoDB</p>
-                  </StyledLogos>
-                </a>
-                <a
-                  href='https://expressjs.com/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/express.svg'
-                      alt='express'
-                      aria-label='express icon'
-                      tabIndex='0'
-                    />
-                    <p>Express</p>
-                  </StyledLogos>
-                </a>
-                <a href='https://nodejs.org/' target='_blank' rel='noreferrer'>
-                  <StyledLogos>
-                    <img
-                      src='images/node-js.svg'
-                      alt='node'
-                      aria-label='node icon'
-                      tabIndex='0'
-                    />
-                    <p>Node Js</p>
-                  </StyledLogos>
-                </a>
-                <a
-                  href='https://sass-lang.com/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/sass.svg'
-                      alt='sass'
-                      aria-label='sass icon'
-                      tabIndex='0'
-                    />
-                    <p>Sass</p>
-                  </StyledLogos>
-                </a>
-                <a
-                  href='https://www.cplusplus.com/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  <StyledLogos>
-                    <img
-                      src='images/c.svg'
-                      alt='c++'
-                      aria-label='c++ icon'
-                      tabIndex='0'
-                    />
-                    <p>C++</p>
-                  </StyledLogos>
-                </a>
-                {/* <StyledLogos>
-                <img
-                  src='images/php.svg'
-                  alt='php'
-                  aria-label='php icon'
-                  tabIndex='0'
-                />
-                <p>PHP</p>
-              </StyledLogos> */}
-              </Fade>
-            </StyledLogosWrapper>
-          </StyledRight>
-        </StyledContainer>
+            </StyledLeft>
+            <StyledRight>
+              <H3>Skills</H3>
+              <StyledLogosWrapper>
+                <Fade cascade damping='0.25'>
+                  <a
+                    href='https://developer.mozilla.org/en-US/docs/Glossary/HTML5'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/html5.svg'
+                        alt='html'
+                        aria-label='html icon'
+                        tabIndex='0'
+                      />
+                      <p>Html5</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://developer.mozilla.org/en-US/docs/Web/CSS'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/css3.svg'
+                        alt='css3'
+                        aria-label='css3 icon'
+                        tabIndex='0'
+                      />
+                      <p>CSS3</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://www.javascript.com/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/javascript.svg'
+                        alt='javascript'
+                        aria-label='javascript icon'
+                        tabIndex='0'
+                      />
+                      <p>JavaScript</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://reactjs.org/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/react.svg'
+                        alt='react'
+                        aria-label='react icon'
+                        tabIndex='0'
+                      />
+                      <p>React</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://www.mongodb.com/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/mongodb.svg'
+                        alt='mongodb'
+                        aria-label='mongodb icon'
+                        tabIndex='0'
+                      />
+                      <p>MongoDB</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://expressjs.com/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/express.svg'
+                        alt='express'
+                        aria-label='express icon'
+                        tabIndex='0'
+                      />
+                      <p>Express</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://nodejs.org/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/node-js.svg'
+                        alt='node'
+                        aria-label='node icon'
+                        tabIndex='0'
+                      />
+                      <p>Node Js</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://sass-lang.com/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/sass.svg'
+                        alt='sass'
+                        aria-label='sass icon'
+                        tabIndex='0'
+                      />
+                      <p>Sass</p>
+                    </StyledLogos>
+                  </a>
+                  <a
+                    href='https://www.cplusplus.com/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <StyledLogos>
+                      <img
+                        src='images/c.svg'
+                        alt='c++'
+                        aria-label='c++ icon'
+                        tabIndex='0'
+                      />
+                      <p>C++</p>
+                    </StyledLogos>
+                  </a>
+                </Fade>
+              </StyledLogosWrapper>
+            </StyledRight>
+          </StyledContainer>
+        </div>
       </StyledAboutMe>
     </StyledAboutMeWrapper>
   )
