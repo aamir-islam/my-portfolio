51c8674a611e453f609d24fda3816ea278fbe751
AryamanSingh <aryamansingh2803@gmail.com>
2022-03-25 00:38:22 +0530

    css fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,8 +11,8 @@ const Main = ({ theme, isOpen }) => {
         boxSizing: 'border-box',
         filter: isOpen ? 'blur(1px) brightness(0.6)' : 'blur(0px)',
         backgroundColor: theme === 'lightTheme' ? '#171A23' : '#FFFFFF',
-        transition: 'background-color 0.5s linear',
-        transform: ' scale(1.01)',
+        transition: 'background-color 0.5s linear, transform 0.15s linear',
+        transform: isOpen ? 'scale(1.005)' : 'scale(1)',
       }}
     >
       <LandingPage theme={theme} />
